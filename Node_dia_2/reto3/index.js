6ba2cdbc6a12219a2fd082cd07cdedd2ec780b6e
Jose Alejandro Perez Mercado <110130517+AlejoPrzHub@users.noreply.github.com>
2022-11-15 12:21:33 +0100

    Delete Node_dia_2 directory

--- Node_dia_2/reto3/index.js
+++
@@ -1,5 +0,0 @@
-let write = require("./write")
-let read = require("./read")
-
-//write.writeAndRead("reto3.json",{"calle":"madrid","numero":6})  //metodo fs/promises
-read.readConsole(console.log());
