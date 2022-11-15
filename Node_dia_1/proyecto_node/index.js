b92319b282ea3523cf0a9d90e0a300d53bb1fe5d
Jose Alejandro Perez Mercado <110130517+AlejoPrzHub@users.noreply.github.com>
2022-11-15 12:21:25 +0100

    Delete Node_dia_1 directory

--- Node_dia_1/proyecto_node/index.js
+++
@@ -1,6 +0,0 @@
-let ver = require("./readConsole");
-let leer = require("./writeAndReadObject");
-
-//ver.readConsole(console.log())                                               //metodo readline
-
-// leer.writeAndRead("reto4.json",{"calle":"madrid","numero":6})                //metodo manual
