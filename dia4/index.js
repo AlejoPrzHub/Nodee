ee3d57b779a953bb3487303cf838267327aa2789
AlejoPrzHub <josealeprz@gmail.com>
2022-11-17 14:28:49 +0100

    "cambios"

--- dia4/index.js
+++ dia4/index.js
@@ -5,17 +5,31 @@ function consultarPokemon()
     .then(function(response){
         response.json()
         .then(function(pokemon)
-        {
-            document.getElementById("Nombre").innerHTML = 
-            `<h2>${pokemon.name}</h2>`
-            
-            // document.getElementById("Imagen").innerHTML = 
-            // `<img src="${pokemon.sprites.other.dream_world.front_default}"`
+        
+        {   
+            //imagen
             document.getElementById("Imagen").innerHTML = 
             `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${document.getElementById("Pokemon").value}.svg">`
             
 
+            // `<img src="${pokemon.sprites.other.dream_world.front_default}"`
+
+            // console.log(pokemon.sprites.other.dream_world.front_default);
             //console.log(pokemon.sprites.other.dream_world.front_default);
+
+            //nombre
+            document.getElementById("Nombre").innerHTML = 
+            `<h2>${pokemon.name}</h2>`
+            
+            
+            
+            //habilidades
+            document.getElementById("Habilidades").innerHTML =
+            `<h4>${pokemon.abilities[0].ability.name}</h4>
+            <h4>${pokemon.abilities[1].ability.name}</h4>`
+            
+
+            
 
             
            // console.log(pokemon.abilities)
